ad361e2ec1b9c6c2b9d9cfa7c80cbffebf118934
alexander.guaman.cruz@gmail.com <alexander.guaman.cruz@gmail.com>
2020-07-14 21:43:00 -0500

    First Class

---
+++ variables.js
@@ -0,0 +1,9 @@
+var nombre = 'Alexander', primerApellido = 'Guamán', segundoApellido = 'Cruz'
+var edad = 44
+
+edad = '28 años'
+
+console.log(`Hola ${nombre} ${primerApellido} ${segundoApellido}`)
+console.log(`Tengo ${edad}`)
+
+var peso = 70
