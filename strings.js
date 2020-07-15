9d0cad82b41b3db9b33e2148e83696b3641fddcf
alexander.guaman.cruz@gmail.com <alexander.guaman.cruz@gmail.com>
2020-07-14 22:18:41 -0500

    Second Class

---
+++ strings.js
@@ -0,0 +1,15 @@
+var nombre = 'Alexander', primerApellido = 'Guamán', segundoApellido = 'Cruz'
+
+var nombreEnMayusculas = nombre.toUpperCase() 
+
+var primerApellidoEnMinusculas = primerApellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${primerApellido.toUpperCase()} ${segundoApellido.toLowerCase()}`
+
+var str = nombre.substr(1, 2)
+
+var strUltimo = nombre.substr(-1)
